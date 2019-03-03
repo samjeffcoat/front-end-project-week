a8b9db0168bf197bc2e1ab9a006090c8e55b99a7
samjeffcoat <samjeffcoat@gmail.com>
2019-03-03 12:28:38 -0600

    Makes axios call and gets list od notes. Notes are successfully in dev tools. Now need to get them to display

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-
+import ReactDOM from 'react-dom';
 import './App.css';
+import axios from 'axios';
+
+
+
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      notes:[],
+      note:{
+        tags : ' ',
+        _id: ' ',
+        title: ' ',
+        textBody: ' ',
+        __v: ' '
+      }
+    }
+  }
+  componentDidMount() {
+    axios.get('https://fe-notes.herokuapp.com/note/get/all')
+    .then(response => this.setState({ items: response.data}))
+    .catch(error => console.log(error));
+  }
   render() {
     return (
       <div className="App">
@@ -10,6 +33,7 @@ class App extends Component {
       </div>
     );
   }
+
 }
 
 export default App;
