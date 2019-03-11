e688fcf3f727b42b198bebc493f286633d8662d4
samjeffcoat <samjeffcoat@gmail.com>
2019-03-10 22:06:19 -0500

    Correct id is now following the notes, need to get them to actually edit and delete

--- notes/src/components/CardComponent.js
+++ notes/src/components/CardComponent.js
@@ -13,6 +13,16 @@ const Card = props => {
       </div>
     );
   }
+  return (
+    <div className="card-container-main">
+      <div className="title-container-main">
+        <h2>{props.note.title}</h2>
+      </div>
+      <div className="body-container">
+        <p>{props.note.textBody}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
