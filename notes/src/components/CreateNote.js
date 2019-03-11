b1e0be25ff43366acf2e990bb6939a02dce33e36
samjeffcoat <samjeffcoat@gmail.com>
2019-03-10 22:25:46 -0500

    takes axios call out of create note and it still works

--- notes/src/components/CreateNote.js
+++ notes/src/components/CreateNote.js
@@ -1,50 +1,46 @@
 import React, { Component } from "react";
 
 import { Form, Input, Button } from "reactstrap";
-import Axios from "axios";
 
 class CreateNote extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: " ",
-      textBody: " "
+      note: {
+        title: " ",
+        textBody: " "
+      }
     };
   }
 
-  addNote = () => {
-    Axios.post(`https://fe-notes.herokuapp.com/note/create`, this.state)
-      .then(res => {
-        this.setState({ notes: res.data });
-      })
-      .then(() => {
-        this.props.history.push("/");
-      });
+  addNote = e => {
+    e.preventDefault();
+    this.props.newNote(this.state.note);
+    this.props.history.push("/");
   };
 
   handleInputChange = e => {
     e.preventDefault();
     this.setState({
-      [e.target.name]: e.target.value
+      note: { ...this.state.note, [e.target.name]: e.target.value }
     });
   };
 
   render() {
     return (
       <Form>
+        <h2>Create New Note!</h2>
         <Input
           onChange={this.handleInputChange}
           placeholder="Title"
           name="title"
           type="text"
-          value={this.state.title}
         />
         <Input
           type="textbody"
           onChange={this.handleInputChange}
           placeholder="Text Body"
           name="textBody"
-          value={this.state.textBody}
         />
         <br />
         <Button onClick={this.addNote}>Save</Button>
